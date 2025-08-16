a35593dd8322c854b525d3c3e0330642635a3dec
ihsan <ihsan@MacBook-Pro-ihsan.local>
2025-08-16 20:50:34 +0600

    Save changes to main.css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,9 +2,17 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="contact-wrapper">
+    <div className="container">
+      <div className="contact-wrapper">
+        <div className="contact_box">
 
+        </div>
+        <div className="contact_box">
+
+        </div>
+      </div>
     </div>
+
   );
 };
 
